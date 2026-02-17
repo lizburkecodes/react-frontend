0fd3f440a2fe7c7d75c30ed97bc013f9b8526ebe
Liz Burke <liz.burke.code@gmail.com>
2026-02-17 12:06:13 -0500

    delete

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,33 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
-const Product = ({ product }) => {
+const Product = ({ product, getProducts }) => {
+
+  const deleteProduct = async (id) => {
+    const result = await Swal.fire({
+      title: "Confirmation",
+      text: "Are you sure you want to delete this product?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    });
+
+    if (result.isConfirmed) {
+      try {
+        await axios.delete(`${import.meta.env.VITE_API_URL}/api/products/${id}`);
+        toast.success("Product deleted successfully!");
+        getProducts();
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
+
+  }
+
   return (
     <div className="bg-white rounded shadow-lg overflow-hidden">
         <img src={product.image} className="w-full h-28 object-cover" />
@@ -10,7 +37,7 @@ const Product = ({ product }) => {
             <div className="text-sm">Price: ${product.price}</div>
           <div className="mt-2 flex gap-4">
             <Link to={`/edit/${product._id}`} className="inline-block w-full text-center shadow-md text-sm bg-gray-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-gray-600 hover:cursor-pointer">Edit</Link>
-            <Link to={`/delete/${product._id}`} className="inline-block w-full text-center shadow-md text-sm bg-red-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-red-600 hover:cursor-pointer">Delete</Link>
+            <button onClick={() => deleteProduct(product._id)} className="inline-block w-full text-center shadow-md text-sm bg-red-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-red-600 hover:cursor-pointer">Delete</button>
         </div>  
         </div>
     </div>
