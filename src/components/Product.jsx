f87ef0a7466e1e7436f85e1d608a8a457598a56c
Liz Burke <liz.burke.code@gmail.com>
2026-02-19 12:59:55 -0500

    replace price attr

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -34,7 +34,7 @@ const Product = ({ product, getProducts }) => {
         <div className="px-4 pt-2 pb-4">
             <h2 className="text font-semibold">{product.name}</h2>
             <div className="text-sm">Quantity: {product.quantity}</div>
-            <div className="text-sm">Price: ${product.price}</div>
+            <div className="text-sm">Location: {product.location}</div>
           <div className="mt-2 flex gap-4">
             <Link to={`/edit/${product._id}`} className="inline-block w-full text-center shadow-md text-sm bg-gray-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-gray-600 hover:cursor-pointer">Edit</Link>
             <button onClick={() => deleteProduct(product._id)} className="inline-block w-full text-center shadow-md text-sm bg-red-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-red-600 hover:cursor-pointer">Delete</button>
